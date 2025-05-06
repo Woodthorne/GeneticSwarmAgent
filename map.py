93e1dc181c0813e42db7f722080c2a8866e512ec
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-06 10:10:27 +0200

    Initial commit

---
+++ map.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from signals import PixelEnum
+from utils import drill, get_color
+
+class Map:
+    def __init__(self, map_name: str) -> None:
+        self._img_path = Path('maps') / map_name
+        self._img: np.ndarray = cv2.imread(self._img_path)
+
+    @property
+    def img(self) -> np.ndarray:
+        return self._img.copy()
+    
+    @property
+    def img_path(self) -> Path:
+        return self._img_path
+    
+    def check_position(self, position: list[int]) -> PixelEnum:
+        found = self._img
+        for axis in position:
+            found = found[axis]
+
+        return get_color(found)
+    
+    def find_positions(self, color: PixelEnum) -> list[list[int]]:
+        positions = [position for position, pixel in drill(self._img)
+                     if get_color(pixel) == color]
+        return positions
+    
