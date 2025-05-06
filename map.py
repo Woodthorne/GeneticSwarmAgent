71456bdb439fe6c0c69b10880d61b33456cb2f05
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-06 10:31:16 +0200

    Added axes property

--- map.py
+++ map.py
@@ -10,6 +10,7 @@ class Map:
     def __init__(self, map_name: str) -> None:
         self._img_path = Path('maps') / map_name
         self._img: np.ndarray = cv2.imread(self._img_path)
+        self._axes: int = len(self.img.shape) - 1
 
     @property
     def img(self) -> np.ndarray:
@@ -18,6 +19,10 @@ class Map:
     @property
     def img_path(self) -> Path:
         return self._img_path
+
+    @property
+    def axes(self) -> int:
+        return self._axes
     
     def check_position(self, position: list[int]) -> ColorEnum:
         found = self._img
