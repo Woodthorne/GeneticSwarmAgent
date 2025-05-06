61cd3661678a7344c0442aa20ffb8095da6fd5f3
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-06 09:19:43 +0200

    Initial commit

---
+++ utils.py
@@ -0,0 +1,38 @@
+from functools import cache
+from typing import Iterator
+
+import numpy as np
+
+from signals import PixelEnum
+
+
+@cache
+def add(*vectors: list[int]) -> list[int]:
+    assert all(len(vectors[0]) == len(vector) for vector in vectors), \
+        'All vectors need to be of same length.'
+    
+    vector = [sum(vals) for vals in zip(*vectors)]
+    return vector
+
+
+def drill(array: np.ndarray) -> Iterator[tuple[list[int], np.ndarray]]:
+    subarray: np.ndarray
+    for index, subarray in enumerate(array):
+        if len(subarray.shape) == 1:
+            yield ([index], subarray)
+        else:
+            for result in drill(subarray):
+                result[0].insert(0, index)
+                yield result
+
+
+def get_colour(array: np.ndarray) -> PixelEnum:
+    if all(array == [255, 255, 255]):
+        return PixelEnum.WHITE
+    if all(array == [000, 000, 000]):
+        return PixelEnum.BLACK
+    if all(array == [000, 000, 255]):
+        return PixelEnum.RED
+    if all(array == [000, 255, 000]):
+        return PixelEnum.GREEN
+    raise ValueError(f'Unrecognised colour: {array}')
