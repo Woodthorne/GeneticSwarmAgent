a2e4f331aa8633107bceaab71d75f0c4191c040c
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-06 08:40:35 +0200

    Initial commit

---
+++ particle.py
@@ -0,0 +1,52 @@
+from typing import Callable
+
+from utils import add
+
+class Particle:
+    def __init__(
+            self,
+            id_num: int,
+            position: list[int],
+            velocity: list[int],
+            fitness_func: Callable
+    ) -> None:
+        self._id = id_num
+        self._position = position
+        self._fitness_func = velocity
+        self._fitness = fitness_func(position)
+        self._best_position = position
+        self._best_fitness = self.fitness
+        self._velocity = velocity
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def position(self) -> list[int]:
+        return self._position
+    
+    @property
+    def fitness(self) -> float:
+        return self._fitness
+    
+    @property
+    def best_position(self) -> list[int]:
+        return self._best_position
+
+    @property
+    def best_fitness(self) -> list[int]:
+        return self._best_fitness
+    
+    @property
+    def velocity(self) -> list[int]:
+        return self._velocity
+    
+    def move(self, new_velocity: list[float]) -> None:
+        self._position = add(self.position, new_velocity)
+        self._velocity = new_velocity
+        self._fitness = self._fitness_func(self.position)
+        if self.fitness > self.best_fitness:
+            self._best_fitness = self.fitness
+            self._best_position = self.position
+        
