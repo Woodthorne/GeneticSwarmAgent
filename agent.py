2f7d20648bdde52d85001bd0854ee17da5b7d08a
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-07 14:27:53 +0200

    Implemented dumb agent, only seeking destination

--- agent.py
+++ agent.py
@@ -1,14 +1,28 @@
+import random
 from typing import Callable, Iterable
+
+import numpy as np
+
+from utils import distance
 
 
 class Agent:
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            map_shape: tuple[int],
+            target_area: np.ndarray
+    ) -> None:
+        self._map_shape = map_shape
+        self._target_area = target_area
+    
+    @property
+    def target_area(self) -> np.ndarray:
+        return self._target_area.copy()
 
     def new_fitness_func(self, percept: dict) -> Callable:
+        goal = random.choice(self._target_area)
         # TODO: Use genetics to determine fitness based on percept
         def func(position: Iterable) -> float:
-            fitness = 0
-
-            return fitness
+            return -distance(position, goal)
         return func
+    
--- environment.py
+++ environment.py
@@ -22,19 +22,14 @@ class Environment:
         self._sensor_radius: int = sensor_radius
         self._is_done: bool = False
         self._latest_percept: dict[str, np.ndarray|list[list[int]]] = {}
-        self._latest_percept['view'] = np.full(self.map.img.shape, 100)
-        self._target_positions = self.map.find_positions(ColorEnum.GREEN)
-    
-    @property
-    def map(self) -> Map:
-        return self._map
+        self._latest_percept['view'] = np.full(self._map.img.shape, 100)
     
     @property
     def is_done(self) -> bool:
         return self._is_done
     
     def populate_swarm(self, count: int) -> None:
-        starting_positions = self.map.find_positions(ColorEnum.BLUE)
+        starting_positions = self._map.find_positions(ColorEnum.BLUE)
         for particle in self._swarm:
             mask = (starting_positions != particle.position).any(axis = 1)
             starting_positions = starting_positions[mask]
@@ -59,16 +54,16 @@ class Environment:
         percept['view'] = self._latest_percept['view']
         
         percept['swarm_pos'] = np.array([particle.position for particle in self._swarm])
-        # percept['view'] = np.full(self.map.img.shape, 100)
+        # percept['view'] = np.full(self._map.img.shape, 100)
         for particle in self._swarm:
             mask = [slice(max(axis - self._sensor_radius, 0),
                           axis + self._sensor_radius + 1)
                     for axis in particle.position]
-            percept['view'][*mask] = self.map.img[*mask] # TODO: Fix conversion from [0,0,0] to ColorEnum
+            percept['view'][*mask] = self._map.img[*mask] # TODO: Fix conversion from [0,0,0] to ColorEnum
         
         self._latest_percept = percept
         fitness_func = self._agent.new_fitness_func(percept)
-        self._swarm.sort(key=fitness_func, reverse=True)
+        self._swarm.sort(key=lambda p: p.fitness, reverse=True)
         
         # TODO: set variabes
         inertia: float = random.random()
@@ -76,6 +71,7 @@ class Environment:
         exploitation: float = random.random()
 
         best_position = self._swarm[0].best_position
+        print(self._swarm[0].fitness)
         for particle in self._swarm:
             new_velocity = (
                 inertia * particle.velocity \
@@ -83,12 +79,15 @@ class Environment:
                 + exploitation * random.random() * (best_position - particle.position)
             ).astype(np.int64)
             particle.move(new_velocity, fitness_func)
-            assert all([0 for _ in self.map.axes] < particle.position) \
-                 and all(particle.position < self.map.axes), \
-                    f'Collision at {particle.position}'
-            percept['view'][*particle.position] = [0, 0, 255]
+            # assert all([0 for _ in self._map.axes] < particle.position) \
+            #      and all(particle.position < self._map.axes), \
+            #         f'Collision at {particle.position}'
+            try:
+                percept['view'][*particle.position] = [0, 0, 255]
+            except IndexError:
+                pass
 
-        if all(particle.position in self._target_positions for particle in self._swarm):
+        if all(particle.position in self._agent.target_area for particle in self._swarm):
             self._is_done = True
-
+        
         return percept['view']
