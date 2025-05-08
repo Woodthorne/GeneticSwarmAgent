21215197263206994e7c27ef06b4dffb3e36c260
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-08 10:20:41 +0200

    a* implementation

--- agent.py
+++ agent.py
@@ -1,9 +1,12 @@
+import heapq
 import random
+from collections import defaultdict
 from typing import Callable, Iterable
 
 import numpy as np
 
-from utils import distance
+from signals import ColorEnum
+from utils import euclidean, get_color
 
 
 class Agent:
@@ -20,9 +23,62 @@ class Agent:
         return self._target_area.copy()
 
     def new_fitness_func(self, percept: dict) -> Callable:
-        goal = random.choice(self._target_area)
+        destination = random.choice(self._target_area)
+        origin = np.sum(percept['swarm_pos'], axis = 0)
+        goal = self.a_star(origin, destination, percept['view'])
         # TODO: Use genetics to determine fitness based on percept
         def func(position: Iterable) -> float:
-            return -distance(position, goal)
+            return -euclidean(position, goal)
         return func
     
+    def a_star(self, origin: np.ndarray, destination: np.ndarray, map_: np.ndarray):
+        frontier = []
+        heapq.heappush(frontier, (0, origin))
+        prev_pos = {tuple(origin): None}
+        
+        current_cost = defaultdict(lambda: float('inf'))
+        current_cost[tuple(origin)] = 0
+
+        reachable_dest = None
+        min_distance = float('inf')
+
+        while frontier:
+            _, current = heapq.heappop(frontier)
+            distance = euclidean(current, destination)
+            if distance < min_distance:
+                reachable_dest = current
+                min_distance = distance
+            
+            if tuple(current) == tuple(destination):
+                break
+
+            for movement in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
+                neighbor = current + movement
+                if not all([0 for _ in self._map_shape] < neighbor) \
+                or not all(neighbor < self._map_shape):
+                    continue
+
+                location = map_[*neighbor]
+                if get_color(location) == ColorEnum.BLACK:
+                    continue
+
+                new_cost = current_cost[tuple(current)] + 1
+                if new_cost < current_cost[tuple(neighbor)]:
+                    current_cost[tuple(neighbor)] = new_cost
+                    priority = new_cost + euclidean(neighbor, destination)
+                    heapq.heappush(frontier, (priority, neighbor))
+                    prev_pos[tuple(neighbor)] = current
+        
+            route = []
+            current = destination
+            while np.all(current):
+                route.append(current)
+                current = prev_pos.get(tuple(current))
+            route.reverse()
+
+            if not route or np.all(route[0] != origin):
+                route = [origin]
+                if np.all(reachable_dest):
+                    route.append(reachable_dest)
+            
+            return route
