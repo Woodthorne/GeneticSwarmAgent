654c4eeb88458c8be3beb03a003370b0ffd9718e
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-05-21 11:47:15 +0200

    Removed ImageMap

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import numpy as np
 from agent import Agent
 from drone import Drone
 from environment import Environment
-from map import AbstractMap, ImgMap
-from signals import ColorEnum
+from map import AbstractMap
 
 
 def main(
@@ -16,20 +15,11 @@ def main(
         collisions: bool = True,
         sensor_radius: int = 10
 ) -> None:
-    # if map_name:
-    #     map_ = ImgMap(map_name = map_name)
-    #     agent = Agent(
-    #         map_shape = map_.axes,
-    #         target_area = map_.find_positions(ColorEnum.GREEN),
-    #         map_type = type(map_)
-    #     )
-    # else:
     map_ = AbstractMap(**map_params)
     
     agent = Agent(
-        map_shape = map_.axes,
         target_area = map_.goal,
-        map_type = type(map_)
+        step_size = 0.9 * sensor_radius
     )
 
     environment = Environment(
@@ -47,19 +37,29 @@ def main(
     #         fitness_func=np.sum
     #     )
     #     environment._swarm.append(drone)
+    # drone = Drone(
+    #     id_num = 999,
+    #     position=np.array((sensor_radius, sensor_radius)),
+    #     velocity=np.array((0,0)),
+    #     fitness_func=np.sum
+    # )
+    # environment._swarm.append(drone)
     
     environment.populate_swarm(swarm_size)
     
     cycles = 0
+    time_taken = 0
     while not environment.is_done:
         cycles += 1
         print('Running cycle', cycles)
         cycle_start = default_timer()
         new_frame = environment.step()
         cycle_time = round(default_timer() - cycle_start, 3)
-        print(f'Cycle {cycles} complete. Elapsed time: {cycle_time} s')
+        time_taken += cycle_time
+        avg_fps = round(cycles / time_taken, 3)
+        print(f'Cycle {cycles} complete. Elapsed time: {cycle_time} s ({avg_fps} fps)')
         cv2.imshow('frame', new_frame.astype(np.uint8))
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     if environment.is_done:
         print('Simulation complete. Press Q to close.')
@@ -68,8 +68,6 @@ def main(
 
 
 if __name__ == '__main__':
-    map_name = 'sample.png'
-    
     abstract_map_params = dict(
         axes = (150, 200),
         start_zone = ((10, 10), (50, 50)),
@@ -78,7 +76,8 @@ if __name__ == '__main__':
             ((20, 100), (130, 100))
         ]   
     )
+    sensor_radius=min(abstract_map_params['axes']) / 10
     # map_name = None
     main(map_params=abstract_map_params,
-         collisions=False,
-         sensor_radius=50)
+         collisions=True,
+         sensor_radius=sensor_radius)
